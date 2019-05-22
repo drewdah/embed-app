ada968443cfab0176d7759ccdc4102912e9e5e00
Drew Lawton <drewdah@gmail.com>
2019-05-22 15:09:15 -0700

    fixed domain in CSP

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(csp({
   directives: {
-    frameAncestors: ["'drewlawton.com"],
+    frameAncestors: ["'drewlawton.com'"],
     imgSrc: ["'self'"],
     reportUri: `/csp`
   },
